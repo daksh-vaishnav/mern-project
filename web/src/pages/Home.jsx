8e601cfa1045fe5ee207b03c022a1f92d831664a
daksh <daksh@dakshv.com>
2025-04-10 10:19:10 -0700

    hero content

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -8,8 +8,12 @@ export const Home = () => {
             <div className='hero-container'>
                 <img className='hero-banner' src={heroBanner} alt="" />
                 <div className='card'>
-                    <h2>Sale ends today</h2>
+                    <h2 className='font1'>Sale ends today</h2>
                     <p className='hero-text'>Have a dream? Start learning your way to it with courses from ₹499.</p>
+                </div>
+                <div className='hero-content'>
+                    <h2 className='hero-title font1'>All the skills you need in one place</h2>
+                    <p className='hero-text'>From critical skills to technical topics, Udemy supports your professional development.</p>
                 </div>
             </div>
         </section>
